9cf2b351c55266a9d85eaccab180872b4f8844a9
ckorkmaz <cansukorkmaz57@gmail.com>
2021-08-12 23:31:22 +0300

    MainPage and Pageobject added

--- src/test/java/YemekSepetiStepDefinitions/YemekSepetiStepDefinitions.java
+++ src/test/java/YemekSepetiStepDefinitions/YemekSepetiStepDefinitions.java
@@ -1,16 +1,44 @@
 package YemekSepetiStepDefinitions;
 
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.concurrent.TimeUnit;
 
 public class YemekSepetiStepDefinitions {
 
+    protected WebDriver driver;
+    @Before
+    public void beforeTest() {
+        System.setProperty("webdriver.chrome.driver", "webdriver/chromedriver.exe");
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("start-maximized")
+                .addArguments("--disable-notifications");
+
+        driver = new ChromeDriver(chromeOptions);
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+
+    }
+
+    @After
+    public void afterTest() {
+
+        driver.close();
+        driver.quit();
+    }
+
     @Given("User navigate to {string} page")
-    public void userNavigateToPage(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void userNavigateToPage(String endpoint) {
+
+        driver.get(endpoint);
+
     }
 
     @When("^User enters (.*) and (.*)$")
